130508b7b6c004e4eae94cb743da911f63fa54f9
devmcdonald <dpm360@nyu.edu>
2023-11-20 03:11:11 +0000

    modified:   Milestone 1/Attorney General Merrick Garland The 60 Minutes Interview transcript.txt 	modified:   Milestone 1/Charles Barkley The 60 Minutes Interview transcript.txt 	modified:   Milestone 1/Checkpoint 1.py 	modified:   Milestone 1/Deion Sanders The 2023 60 Minutes Interview transcript.txt 	modified:   Milestone 1/Gen. Mark Milley The 60 Minutes Interview transcript.txt 	modified:   Milestone 1/Godfather of AI Geoffrey Hinton The 60 Minutes Interview transcript.txt 	modified:   Milestone 1/President Joe Biden The 2023 60 Minutes Interview transcript.txt 	modified:   Milestone 1/README.md 	modified:   Milestone 1/Rich Paul The 60 Minutes Interview transcript.txt 	modified:   Milestone 1/Volodymyr Zelenskyy The 2023 60 Minutes Interview transcript.txt 	modified:   Milestone 1/World Number 1 Pool Player Shane Van Boening The 60 Minutes Interview transcript.txt 	modified:   "Milestone 1/Yannick N\303\251zet-S\303\251guin The 60 Minutes Interview transcript.txt" 	modified:   Milestone 2/Attorney General Merrick Garland The 60 Minutes Interview.txt 	modified:   Milestone 2/Charles Barkley The 60 Minutes Interview.txt 	modified:   Milestone 2/Checkpoint 2.py 	modified:   Milestone 2/Deion Sanders The 2023 60 Minutes Interview.txt 	modified:   Milestone 2/Gen Mark Milley The 60 Minutes Interview.txt 	modified:   Milestone 2/Godfather of AI Geoffrey Hinton The 60 Minutes Interview.txt 	modified:   Milestone 2/President Joe Biden The 2023 60 Minutes Interview.txt 	modified:   Milestone 2/README.md 	modified:   Milestone 2/Rich Paul The 60 Minutes Interview.txt 	modified:   Milestone 2/Volodymyr Zelenskyy The 2023 60 Minutes Interview.txt 	modified:   Milestone 2/World Number 1 Pool Player Shane Van Boening The 60 Minutes Interview.txt 	modified:   "Milestone 2/Yannick N\303\251zet-S\303\251guin The 60 Minutes Interview.txt" 	modified:   Milestone 3/Checkpoint 3.py 	new file:   Milestone 4/Attorney General Merrick Garland The 60 Minutes Interview Audio.wav 	new file:   Milestone 4/Charles Barkley The 60 Minutes Interview Audio.wav 	new file:   Milestone 4/Checkpoint 4.py 	new file:   Milestone 4/Deion Sanders The 2023 60 Minutes Interview Audio.wav 	new file:   Milestone 4/Gen Mark Milley The 60 Minutes Interview Audio.wav 	new file:   Milestone 4/Godfather of AI Geoffrey Hinton The 60 Minutes Interview Audio.wav 	new file:   Milestone 4/Rich Paul The 60 Minutes Interview Audio.wav 	new file:   "Milestone 4/Yannick N\303\251zet-S\303\251guin The 60 Minutes Interview Audio.wav" 	deleted:    Milestone 4/train.py 	deleted:    Milestone 4/train_vocoder.py

---
+++ Milestone 4/Checkpoint 4.py
@@ -0,0 +1,38 @@
+from TTS.api import TTS
+import torch
+
+#Get device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+#Specify language
+lang = "es"
+
+#Load model for inference
+model_name = f"tts_models/{lang}/mai/tacotron2-DDC"
+
+#Path to translated text files
+titles = ["/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Attorney General Merrick Garland The 60 Minutes Interview.txt", 
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Charles Barkley The 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Deion Sanders The 2023 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Gen Mark Milley The 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Godfather of AI Geoffrey Hinton The 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of President Joe Biden The 2023 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Rich Paul The 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Volodymyr Zelenskyy The 2023 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of World Number 1 Pool Player Shane Van Boening The 60 Minutes Interview.txt",
+          "/workspaces/Fall-2023-NYU-AI/Milestone 3/Translation of Yannick Nézet-Séguin The 60 Minutes Interview.txt"]
+
+
+for title in titles:
+    #Create output path
+    OUTPUT_PATH=title.replace("Milestone 3/Translation of ", "Milestone 4/")
+    OUTPUT_PATH=OUTPUT_PATH.replace(".txt", " Audio.wav")
+    
+    # Init TTS with the target model name
+    tts = TTS(model_name=model_name, progress_bar=False).to(device)
+
+    with open(title, 'r') as f:
+        #Read text file into string
+        txt = f.read()
+        #Feed string into model and save output
+        tts.tts_to_file(text=txt, file_path=OUTPUT_PATH)
