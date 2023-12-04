9bbf4e04ed754b5ba25446a82b770f842c5369e2
devmcdonald <144968774+devmcdonald@users.noreply.github.com>
2023-12-04 00:14:00 -0500

    Add files via upload

--- milestone-5/app.py
+++ milestone-5/app.py
@@ -2,14 +2,24 @@ import streamlit as st
 from transformers import *
 from PIL import Image
 from pytube import YouTube
+import openai
 import whisper
 from whisper.utils import get_writer
-import moviepy.editor as mp
+from moviepy.editor import TextClip, CompositeVideoClip, VideoFileClip, AudioFileClip
+from moviepy.video.tools.subtitles import SubtitlesClip
+import os.path
+import time
+from TTS.api import TTS
+import torch
+import re
+import sys
+import pysrt
 
 #Page title
 st.title("Foreign Whispers")
 
 #Youtube link input, download video
+link = ''
 link = st.text_input("YouTube URL: ")
 
 #Only continue with input link
@@ -39,52 +49,86 @@ if link != '':
     if dst != '':
         #Separate audio from video file
         path = path.replace(":", "")
-        video = mp.VideoFileClip(path)
+        video = VideoFileClip(path)
         audio = video.audio
         path=path.replace(".mp4", ".mp3")
         audio.write_audiofile(path)
         engAudioFile = path #to reference in future
 
-
+    
         #Translate audio files to text (English)
         engModel = whisper.load_model("base")
         result = engModel.transcribe(engAudioFile)
+        
         audio = engAudioFile
         output_directory = "/workspaces/Fall-2023-NYU-AI/milestone-5"
-        options = {
-                'max_line_width': None,
-                'max_line_count': None,
-                'highlight_words': False
-            }
-        txt_writer = get_writer("txt", output_directory)
-        txt_writer(result, audio, options)
-        engTextFile = output_directory + '/' + title + '.txt' #to reference in future
+        
+        srt_writer = get_writer("srt", output_directory)
+        srt_writer(result, audio)
+    
+        output_directory = "/workspaces/Fall-2023-NYU-AI/milestone-5"  
+        engTextFile = output_directory + '/' + title + '.srt' #to reference in future
+        engTextFile=engTextFile.replace(":", "")
 
+        translationText=engTextFile.replace(".srt", " translation.srt")
+        
 
+          
         #Load pretrained translation model
         task_name = f"translation_{src}_to_{dst}"
         model_name = f"Helsinki-NLP/opus-mt-{src}-{dst}"
 
         translator = pipeline(task_name, model=model_name, tokenizer=model_name)
 
-
-
-        #Run inference on each line of input text and write NLP output to new file
+        
+        #Run inference on each line of input text and write NLP output to new file         
         with open(engTextFile, 'r') as f:
-            lines = f.readlines()
-            translationText=engTextFile.replace(".txt", " translation.txt")
+            englishLines = f.read()
+            lines = re.split(r'\n\n', englishLines)
+            translatedLines = [translator(line)[0]["translation_text"] + '\n' for line in lines]
+            
+            translationText=engTextFile.replace(".srt", " translation.srt")
             with open(translationText, 'w') as n:
-                for line in lines:
-                    n.write(translator(line)[0]["translation_text"] + '\n')
-                    
-                    
+                for line in translatedLines:
+                    n.write(line)            
+        
+        # Fix srt file output (writing as single line)
+        with open(translationText, 'r') as file:
+            srt_content = file.readlines()
 
+        formatted_subtitles = []
 
+        for entry in srt_content:
+            parts = re.split(r'\s+', entry.strip(), maxsplit=4)  # Split into 5 parts using space as delimiter
+            if len(parts) == 5:
+                sequence_number, time_start, arrow, time_end, text = parts
+                time_start = time_start.replace('.', ',')
+                time_end = time_end.replace('.', ',')
+                formatted_subtitle = f"{sequence_number}\n{time_start} {arrow} {time_end}\n{text}"
+                formatted_subtitles.append(formatted_subtitle)
+
+        # Write the corrected subtitles to the file
+        fixedText = translationText.replace("translation.srt", "fixed translation.srt")
+        with open(fixedText, 'w') as file:
+            file.writelines('\n\n'.join(formatted_subtitles))
+                    
+        with open(fixedText, 'r') as file:
+            lines = file.readlines()
+        lines = lines[:-3]
+        with open(fixedText, 'w') as file:
+            file.writelines(lines)      
+        
+        
+        
+        fixedText = translationText.replace("translation.srt", "fixed translation.srt")
+        #Get device
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        
         #Load model for foreign language inference
         model_name = f"tts_models/{dst}/css10/vits"
 
         #Create output path
-        translationAudio=translationText.replace(".txt", ".wav")
+        translationAudio=fixedText.replace(".srt", ".mp3")
             
         # Init TTS with the target model name
         tts = TTS(model_name=model_name, progress_bar=False).to(device)
@@ -94,3 +138,35 @@ if link != '':
             txt = f.read()
             #Feed string into model and save output
             tts.tts_to_file(text=txt, file_path=translationAudio)
+        
+ 
+        
+        
+        
+        
+        fixedText = f"/workspaces/Fall-2023-NYU-AI/milestone-5/{title} fixed translation.srt"
+        fixedText = fixedText.replace(":", "")
+        
+        translatedAudio = fixedText.replace(".srt", ".mp3")
+        #Add translated subtitles to video
+        output_vid = "/workspaces/Fall-2023-NYU-AI/milestone-5/output_vid.mp4"
+        with open(fixedText, 'r', encoding='utf-8') as file:
+            sub_content = file.read()
+            
+        generator = lambda txt: TextClip(txt, font='Arial', fontsize=28, color='white')
+        subs = SubtitlesClip(fixedText, generator) 
+        result = CompositeVideoClip([video, subs.set_position(("center", "bottom"))])
+
+        
+        # Replace audio of video
+        audio = AudioFileClip(translatedAudio)
+        translatedVideo = result.set_audio(audio)
+        translatedVideo.write_videofile(output_vid)
+        
+        # Upload final video to streamlit
+        st.video(output_vid, format="video/mp4", start_time=0)
+        
+        
+      
+            
+        
