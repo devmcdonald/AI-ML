a9b88c33d5835eb9fd7f568edc3f55aad586f9e2
devmcdonald <dpm360@nyu.edu>
2024-08-23 12:34:40 -0400

    modified:   YouTube_Closed_Captioning_API/app.py

--- YouTube_Closed_Captioning_API/app.py
+++ YouTube_Closed_Captioning_API/app.py
@@ -99,15 +99,14 @@ link = st.text_input("YouTube URL: ")
                 
 # Only continue with input link
 if link:
-
     try:
-        """
+        
         # pytube version
-        vid = YouTube(link, use_oauth=True, allow_oauth_cache=True)
-        sanitized_title = sanitize_filename(vid.title)
-        video_path = f"{sanitized_title}.mp4"
-        vid.streams.filter(progressive="True").get_highest_resolution().download(video_path)
-        """        
+        # vid = YouTube(link, use_oauth=True, allow_oauth_cache=True)
+        # sanitized_title = sanitize_filename(vid.title)
+        # video_path = f"{sanitized_title}.mp4"
+        # vid.streams.filter(progressive="True").get_highest_resolution().download(video_path)
+                
         # pytubefix version
         vid = YouTube(link, on_progress_callback=on_progress)
         sanitized_title = sanitize_filename(vid.title)
